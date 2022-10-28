45584864c4a519e22f4ca183ca94422caec9fddf
Sean-Boateng <116371547+Sean-Boateng@users.noreply.github.com>
2022-10-28 15:54:32 -0400

    Delete person.py

--- person.py
+++
@@ -1,9 +0,0 @@
-from wallet import Wallet
-
-class Person:
-
-    def __init__(self, name_passed, hair_color_passed, wallet_passed):
-        self.name = name_passed
-        self.hair_color = hair_color_passed
-        self.wallet = wallet_passed
-
